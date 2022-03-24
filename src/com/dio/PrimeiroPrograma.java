1372bb0be53c94027198bf834ab32da25a13cf54
Marcos Fernando <marcos.fersh@gmail.com>
2022-03-23 23:54:25 -0300

    criando class Livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -8,7 +8,43 @@ public class PrimeiroPrograma {
 //        int b = 3;
 //        System.out.println("Alo " + (a+b));
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro = new Livro("Livro01", 122);
+        System.out.println(livro.toString());
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
